20cd09e627b700b544b27aa6f4f1548a58d7357b
Alfred Chavez Urday <achavez@localhost.localdomain>
2018-05-03 20:09:34 -0500

    Ej 5

---
+++ ListExe/ej5.h
@@ -0,0 +1,33 @@
+void gauss() {
+  int norm, row, col;  /* Normalization row, and zeroing
+			* element row and col */
+  float multiplier;
+
+  printf("Computing Serially.\n");
+
+  /* Gaussian elimination */
+
+  for (norm = 0; norm < N - 1; norm++) {
+    #pragma omp parallel for shared(A, B) private(multiplier,row,col)
+    for (row = norm + 1; row < N; row++) {
+      multiplier = A[row][norm] / A[norm][norm];
+      for (col = norm; col < N; col++) {
+	         A[row][col] -= A[norm][col] * multiplier;
+      }
+      B[row] -= B[norm] * multiplier;
+    }
+  }
+  /* (Diagonal elements are not normalized to 1.  This is treated in back
+   * substitution.)
+   */
+
+
+  /* Back substitution */
+  for (row = N - 1; row >= 0; row--) {
+    X[row] = B[row];
+    for (col = N-1; col > row; col--) {
+      X[row] -= A[row][col] * X[col];
+    }
+    X[row] /= A[row][row];
+  }
+}
